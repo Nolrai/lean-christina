757490734d2f963d42ebb7048d1a2b3517ba2709
Christina A. Upshaw <chrisaupshaw@gmail.com>
2024-06-28 13:16:49 -0700

    WIP

--- LeanChristina/Basic.lean
+++ LeanChristina/Basic.lean
@@ -1,14 +1,10 @@
 import Mathlib
-import Mathlib.Data.Opposite
-import Mathlib.CategoryTheory.Product
 
 open CategoryTheory CategoryTheory.Category CategoryTheory.Limits Opposite CategoryTheory.Monoidal
 
-variable {A B C : Type} [Category A] [Category B]
+section
 
-structure RatCat (F : A × Bᵒᵖ ⥤ A) where
-  over : A
-  under : B
+variable {A B C : Type} [Category A] [Category B]
 
 abbrev CategoryTheory.Functor.obj₂ (F : A × B ⥤ A) : A → B → A :=
   λ α β => (F.obj ⟨α, β⟩)
@@ -16,16 +12,30 @@ abbrev CategoryTheory.Functor.obj₂ (F : A × B ⥤ A) : A → B → A :=
 def CategoryTheory.Functor.map₂ (F : A × B ⥤ A) {Xl Xr : A} {Yl Yr : B}
   (f : Xl ⟶ Xr)
   (g : Yl ⟶ Yr)
-  : F.obj₂ Xl Yl ⟶ F.obj₂ Xr Yr := F.map (λ p => f ◁ p ▷ g )
+  : F.obj₂ Xl Yl ⟶ F.obj₂ Xr Yr := F.map ⟨f, g⟩
 
+end
 
-def RatCat.mkHom {P : A ⥤ Bᵒᵖ ⥤ A} (σ τ : RatCat P) : Type :=
-  P.obj₂ σ.over (op τ.under) ⟶ P.obj₂ τ.over (op σ.under)
+class PairAction (A : outParam Type) (B) extends Category B where
+  mul : Bᵒᵖ × Bᵒᵖ ⥤ Bᵒᵖ
+  toCategoryA : Category A
+  smul : A × Bᵒᵖ ⥤ A
+  iso : ∀ (a : A) (b₁ b₂ : Bᵒᵖ), smul.obj₂ (smul.obj₂ a b₁) b₂ ≅ smul.obj₂ a (mul.obj₂ b₁ b₂)
 
-def RatCatAux (P : A ⥤ Bᵒᵖ ⥤ A) := ∀ {Xl Xr Y Zl Zr},
+infix:50 " •. " => PairAction.smul.obj₂
+infix:50 " •' " => PairAction.smul.map₂
+
+structure RatCat {A} (B : Type) [PairAction A B] where
+  over : A
+  under : B
+
+def RatCat.mkHom [PairAction A B] (σ τ : RatCat B)  : Type :=
+  σ.over •. (op τ.under) ⟶ τ.over •. (op σ.under)
+
+def RatCatAux [PairAction A B] := ∀ {Xl Xr Y Zl Zr},
   (P.obj₂ (P.obj₂ Xl Y) Zl ⟶ P.obj₂ (P.obj₂ Xr Y) Zr) → (P.obj₂ (P.obj₂ Xl Y) Zl ⟶ P.obj₂ (P.obj₂ Xr Y) Zr)
 
-instance {P : A ⥤ Bᵒᵖ ⥤ A} (aux : RatCatAux P) : CategoryStruct (RatCat P) where
+instance [PairAction] (aux : RatCatAux P) : CategoryStruct (RatCat P) where
   Hom := λ σ τ => RatCat.mkHom σ τ
   id σ := 𝟙 (P.obj₂ σ.over (op σ.under))
   comp {X Y Z} f g := by
@@ -34,3 +44,14 @@ instance {P : A ⥤ Bᵒᵖ ⥤ A} (aux : RatCatAux P) : CategoryStruct (RatCat 
     simp [RatCat.mkHom] at f
     have id_d := 𝟙 <| op (Y.under)
     have := P.map₂ f id_d
+    cases P
+    case mk inst₁ inst₂ P map_id map_comp =>
+    have ⟨obj, map⟩ := P
+    simp [Functor.obj₂, Functor.map₂] at *
+    have ⟨Xu, Xd⟩ := X
+    have ⟨Yu, Yd⟩ := Y
+    have ⟨Zu, Zd⟩ := Z
+    simp [Functor.obj₂, Functor.map₂] at *
+    simp [Functor.obj₂, Functor.map₂] at g
+    simp [Functor.obj₂, Functor.map₂] at f
+    simp [Functor.obj₂, Functor.map₂] at this
